d84d268179d06952890d319aed6e5d16a57ff7b9
mengang-sys <377532225@qq.com>
2020-02-21 19:54:30 +0800

    [mengyang].confirm ack

--- src/main/java/com/my/demo5/Demo5Application.java
+++ src/main/java/com/my/demo5/Demo5Application.java
@@ -1,9 +1,10 @@
 package com.my.demo5;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@EnableRabbit
 @SpringBootApplication
 @MapperScan("com.my.demo5.dao")
 public class Demo5Application {
---
+++ src/main/java/com/my/demo5/receiver/RabbitFanoutListenerService.java
@@ -0,0 +1,45 @@
+package com.my.demo5.receiver;
+
+import com.rabbitmq.client.Channel;
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.*;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+@RabbitListeners({
+        @RabbitListener(
+                bindings = @QueueBinding(
+                        value = @Queue("FanoutQueue-01"),
+                        exchange = @Exchange(value = "FanoutExchange", type = ExchangeTypes.FANOUT),
+                        key = "key.one")),
+
+        @RabbitListener(
+                bindings = @QueueBinding(
+                        value = @Queue("FanoutQueue-02"),
+                        exchange = @Exchange(value = "FanoutExchange", type = ExchangeTypes.FANOUT),
+                        key = "key.two")),
+
+        @RabbitListener(
+                bindings = @QueueBinding(
+                        value = @Queue("FanoutQueue-03"),
+                        exchange = @Exchange(value = "FanoutExchange", type = ExchangeTypes.FANOUT),
+                        key = "key.three")),
+})
+public class RabbitFanoutListenerService {
+
+    //这里只是demo所以直接抛出异常，实际业务要根据自己的业务进行异常捕获，使用basicAck、basicNack、basicReject等方法对消息进行处理，还有幂等处理
+    @RabbitHandler
+    public void getMessage(@Payload String msg, Channel channel, @Headers Map<String, Object> headers) throws Exception {
+
+        long deliveryTag = (Long)headers.get(AmqpHeaders.DELIVERY_TAG);
+        //headers.get(AmqpHeaders.CONSUMER_QUEUE) 打印的是QueueBinding中Queue的值
+        System.out.println("幂等处理 & 业务处理  接收" + headers.get(AmqpHeaders.CONSUMER_QUEUE) + "消费者的消息:" + msg);
+        //消息确认，deliveryTag:该消息的index，multiple：是否批量.true:将一次性ack所有小于deliveryTag的消息。
+        channel.basicAck(deliveryTag, false);
+    }
+}
